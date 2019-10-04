5466f9b41a27fc5824278f73c3f0c71b34256771
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-04 19:54:53 +0200

    created def create_app

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template
 
-from python_org_news import get_python_news
-from weather import weather_by_city
-
-app = Flask(__name__)
+from webapp.python_org_news import get_python_news
+from webapp.weather import weather_by_city
 
 
-@app.route("/")
-def index():
-    title = "Forecast"
-    weather = weather_by_city("Lausanne, Switzerland")
-    news_list = get_python_news()
-    return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    @app.route("/")
+    def index():
+        title = "Forecast"
+        weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
+        news_list = get_python_news()
+        return render_template('index.html', page_title=title, weather=weather,
+                                                news_list=news_list)
+    return app
