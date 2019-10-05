89133ca1aac0534fba57c6d20db5f87d9ef69c46
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-05 19:42:21 +0200

    added basedir

--- webapp/config.py
+++ webapp/config.py
@@ -1,3 +1,8 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db')
 WEATHER_DEFAULT_CITY = 'Lausanne, Switzerland'
 WEATHER_API_KEY = '94df97d243cc4535be7162743191409'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
