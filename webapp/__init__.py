3652bb5e90916336783100afe79079d68291e26d
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-05 19:40:53 +0200

    initiated db

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-
+from webapp.model import db
 from webapp.python_org_news import get_python_news
 from webapp.weather import weather_by_city
 
@@ -7,6 +7,7 @@ from webapp.weather import weather_by_city
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
+    db.init_app(app)
     @app.route("/")
     def index():
         title = "Forecast"
