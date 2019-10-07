98f506a987bc941acbd783448bdb97fd6693074c
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-07 13:27:17 +0200

    added property

--- webapp/model.py
+++ webapp/model.py
@@ -30,3 +30,7 @@ class User(db.Model, UserMixin):
 
     def check_password(self, password):
         return check_password_hash(self.password, password)
+
+    @property
+    def is_admin(self):
+        return self.role == 'admin'
