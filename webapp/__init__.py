1e2d479f17adc7a02fc91ff6fc906be46b181cda
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-07 11:30:22 +0200

    imported News

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template
-from webapp.model import db
-from webapp.python_org_news import get_python_news
+from webapp.model import db, News
 from webapp.weather import weather_by_city
 
 
@@ -12,7 +11,6 @@ def create_app():
     def index():
         title = "Forecast"
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = get_python_news()
-        return render_template('index.html', page_title=title, weather=weather,
-                                                news_list=news_list)
+        news_list = News.query.all()
+        return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
     return app
