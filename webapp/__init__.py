d1dae0ba073f2960a7f4b43dc9f0d2895bb27339
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-07 12:43:40 +0200

    added def login

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask, render_template
 from webapp.model import db, News
 from webapp.weather import weather_by_city
+from webapp.forms import LoginForm
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
+
     @app.route("/")
     def index():
         title = "Forecast"
         weather = weather_by_city(app.config['WEATHER_DEFAULT_CITY'])
-        news_list = News.query.all()
+        news_list = News.query.order_by(News.published.desc()).all()
         return render_template('index.html', page_title=title, weather=weather, news_list=news_list)
+
+    @app.route("/login")
+    def login():
+        title = 'Авторизация'
+        login_form = LoginForm()
+        return render_template('login.html', page_title=title, form=login_form)
     return app
