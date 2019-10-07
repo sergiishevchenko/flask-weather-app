6657f60fdbeb9703f1293548d46e2463d2d87974
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-07 12:44:21 +0200

    added SECRET_KEY

--- webapp/config.py
+++ webapp/config.py
@@ -6,3 +6,5 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'webapp.db'
 WEATHER_DEFAULT_CITY = 'Lausanne, Switzerland'
 WEATHER_API_KEY = '94df97d243cc4535be7162743191409'
 WEATHER_URL = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+
+SECRET_KEY = "sdfqwrefdsfdsafret439589049rjskjkgfj"
