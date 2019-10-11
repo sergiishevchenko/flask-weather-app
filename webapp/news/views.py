2a21f12c0e237511a28d8fd55950267ae2135a92
Sergii Shevchenko <sergiishevchenko017@gmail.com>
2019-10-11 11:48:21 +0200

    added route for add_comment

--- webapp/news/views.py
+++ webapp/news/views.py
@@ -1,6 +1,10 @@
-from webapp.news.models import News
-from flask import abort, Blueprint, render_template, current_app
+from webapp.news.models import Comment, News
+from flask import abort, Blueprint, render_template, current_app, flash, redirect
 from webapp.weather import weather_by_city
+from webapp.news.forms import CommentForm
+from flask_login import current_user
+from webapp.db import db
+from webapp.utils import get_redirect_target
 
 blueprint = Blueprint('news', __name__)
 
@@ -18,5 +22,23 @@ def single_news(news_id):
     my_news = News.query.filter(News.id == news_id).first()
     if not my_news:
         abort(404)
+    comment_form = CommentForm(news_id=my_news.id)
+    return render_template('news/single_news.html', page_title=my_news.title, news=my_news, comment_form=comment_form)
 
-    return render_template('news/single_news.html', page_title=my_news.title, news=my_news)
+
+@blueprint.route('/news/comment', methods=['POST'])
+def add_comment():
+    form = CommentForm()
+    if form.validate_on_submit():
+        comment = Comment(text=form.comment_text.data, news_id=form.news_id.data, user_id=current_user.id)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Комментарий успешно добавлен')
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash('Ошибка в заполнении поля "{}": - {}'.format(
+                    getattr(form, field).label.text,
+                    error
+                ))
+    return redirect(get_redirect_target)
